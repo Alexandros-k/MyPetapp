ce942b4e3ddeb53d2c5c4a3c9f29e40ec571596e
Alexandros-k <alkon134@gmail.com>
2017-03-27 05:59:45 +0300

    Erased package animals cause there was allready a class dog in main package so it was unused. Created a second activity(browseactivity) where i transfer the existed content of mainactivity and in the original i create 3 buttons to access the browseactivity

--- app/src/main/java/com/example/alex/myapplication/MainActivity.java
+++ app/src/main/java/com/example/alex/myapplication/MainActivity.java
@@ -1,107 +1,49 @@
 package com.example.alex.myapplication;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
-import android.widget.ImageView;
-import android.widget.TextView;
-
-import java.util.ArrayList;
 
 
 public class MainActivity extends AppCompatActivity {
 
-
-    int count = 0;
-
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate( Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.mainactivity);
 
-        setContentView(R.layout.activity_main);
-        if (savedInstanceState != null) {
-            count = savedInstanceState.getInt("bla", count);
-        }
-
-        final ArrayList<Dog> dog = new ArrayList<>();
-        Dog d1 = new Dog("Jack", "canis", R.drawable.canis, "dasd");
-        Dog d2 = new Dog("Nick", "colley", R.drawable.colley, "dasd");
-        Dog d3 = new Dog("Rec", "labrador", R.drawable.lab, "dasd");
-
-        dog.add(d1);
-        dog.add(d2);
-        dog.add(d3);
-        final TextView t1 = (TextView) findViewById(R.id.Name);
-        t1.setText(dog.get(count).name);
-        final TextView t2 = (TextView) findViewById(R.id.Race);
-
-        t2.setText(dog.get(count).race);
-        final ImageView i1 = (ImageView) findViewById(R.id.Pic);
-        i1.setImageResource(dog.get(count).image);
-        final TextView t3 = (TextView) findViewById(R.id.Info);
-        t3.setText(dog.get(count).text);
-
-
-        Button button = (Button) findViewById(R.id.button);
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-                count++;
-                if (count > 2) {
-                    count = 0;
-                }
-                final TextView t1 = (TextView) findViewById(R.id.Name);
-                t1.setText(dog.get(count).name);
-                final TextView t2 = (TextView) findViewById(R.id.Race);
-
-                t2.setText(dog.get(count).race);
-                final ImageView i1 = (ImageView) findViewById(R.id.Pic);
-                i1.setImageResource(dog.get(count).image);
-                final TextView t3 = (TextView) findViewById(R.id.Info);
-                t3.setText(dog.get(count).text);
-
-
-
-            }
-
-
-
-        }
+        Button button1 = (Button) findViewById(R.id.button1);
+        button1.setOnClickListener(new View.OnClickListener(){
+                                                 @Override
+                                                 public void onClick(View v) {
+                                                     Intent intent= new Intent("com.example.alex.myapplication.BrowseActivity");
+                                                     startActivity(intent);
+                                                 }
+                                             }
+        );
+        Button button2 = (Button) findViewById(R.id.button2);
+        button2.setOnClickListener(new View.OnClickListener(){
+                                      @Override
+                                      public void onClick(View v) {
+                                          Intent intent= new Intent("com.example.alex.myapplication.BrowseActivity");
+                                          startActivity(intent);
+                                      }
+                                  }
+        );
+        Button button3 = (Button) findViewById(R.id.button3);
+        button3.setOnClickListener(new View.OnClickListener(){
+                                      @Override
+                                      public void onClick(View v) {
+                                          Intent intent= new Intent("com.example.alex.myapplication.BrowseActivity");
+                                          startActivity(intent);
+                                      }
+                                  }
         );
 
-        Button button2 = (Button) findViewById(R.id.button2);
-        button2.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-                count--;
-                if (count < 0) {
-                    count = 2;
-                }
-                final TextView t1 = (TextView) findViewById(R.id.Name);
-                t1.setText(dog.get(count).name);
-                final TextView t2 = (TextView) findViewById(R.id.Race);
-
-                t2.setText(dog.get(count).race);
-                final ImageView i1 = (ImageView) findViewById(R.id.Pic);
-                i1.setImageResource(dog.get(count).image);
-                final TextView t3 = (TextView) findViewById(R.id.Info);
-                t3.setText(dog.get(count).text);
-
-
-            }
-        });
-         
     }
 
-    @Override
-    public void onSaveInstanceState(Bundle savedInstanceState) {
-        super.onSaveInstanceState(savedInstanceState);
-
-        savedInstanceState.putInt("bla", count);
-    }
 }
 
 
