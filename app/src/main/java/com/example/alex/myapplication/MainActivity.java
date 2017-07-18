2c653a53f16b51c936ab6cbc3535b789dbdeae30
Alexandros-k <alkon134@gmail.com>
2017-07-18 16:02:04 +0300

    ui fixes for different screens

--- app/src/main/java/com/example/alex/myapplication/MainActivity.java
+++ app/src/main/java/com/example/alex/myapplication/MainActivity.java
@@ -2,7 +2,6 @@ package com.example.alex.myapplication;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
 import android.view.View;
 
@@ -26,31 +25,31 @@ public class MainActivity extends MenExtender implements FragmentMainActivity.On
 
     @Override
     public void onPetSelected(String onPetLocationInList ) {
-        View fragmentContainer = findViewById(R.id.placeholder);
+        View fragmentContainer = findViewById(R.id.fragment_container);
 
-        /*FragmentBrowseActivity fragment = (FragmentBrowseActivity)
-                getSupportFragmentManager().findFragmentById(R.id.fragPetDetailsId);*/
 
 
         boolean isDualPane = fragmentContainer != null && fragmentContainer.getVisibility() == View.VISIBLE;
 
         if (isDualPane) {
-          /*  FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+         FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
 
-            fragmentTransaction.replace(R.id.placeholder, FragmentBrowseActivity.newInstance(onPetLocationInList));
-            fragmentTransaction.commit();*/
+            fragmentTransaction.replace(R.id.fragment_container, FragmentBrowseActivity.newInstance(onPetLocationInList));
+            fragmentTransaction.commit();
 
-            Fragment newFragment = FragmentBrowseActivity.newInstance(onPetLocationInList);
-            FragmentTransaction transaction =
-                    getSupportFragmentManager().beginTransaction();
 
 // Replace whatever is in the fragment_container view with this
 // fragment and add the transaction to the back stack
-            transaction.replace(R.id.placeholder, newFragment);
-            transaction.addToBackStack(null);
+            /*transaction.replace(R.id.fragment_container, newFragment);
+            transaction.addToBackStack(null);*/
 
 // Commit the transaction
-            transaction.commit();
+
+
+
+            /*FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+            fragmentTransaction.replace(R.id.fragment_container, FragmentBrowseActivity.newInstance(onPetLocationInList));
+            fragmentTransaction.commit();*/
 
 
         }else {
